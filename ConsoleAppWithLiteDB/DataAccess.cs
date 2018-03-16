f89da769c34fd748ad86a2657f081791c3afae29
TheCoder87 <thecoder87@hotmail.com>
2018-03-16 22:14:30 +0530

    Initial commit

---
+++ ConsoleAppWithLiteDB/DataAccess.cs
@@ -0,0 +1,38 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppWithLiteDB
+{
+    public class DataAccess
+    {
+        public static void Create()
+        {
+            using (var db = new LiteDatabase(@"MyData.db"))
+            {
+                var customers = db.GetCollection<Customer>("Customers");
+
+                var newCustomer = new Customer { Id = 1, Name = "TheCoder87", IsActive = true };
+
+                customers.Insert(newCustomer);
+            }
+        }
+
+        public static List<Customer> Read()
+        {
+            using (var db=new LiteDatabase(@"MyData.db"))
+            {
+                List<Customer> cust = new List<Customer>();
+                var c= db.GetCollection<Customer>("Customers").FindAll();
+
+                foreach (var item in c)
+                {
+                    cust.Add(item);
+                }
+
+                return cust;
+
+            }
+        }
+    }
+}
---
+++ ConsoleAppWithLiteDB/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWithLiteDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataAccess.Create();
+
+            var customers = DataAccess.Read();
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Name);
+                Console.WriteLine(customer.IsActive);
+            }
+          
+
+
+            Console.WriteLine("Press any ket to exit...");
+            Console.ReadKey();
+        }
+    }
+}
